9564084be660d767fda36b41970e8a0dc778ebc1
Bing Ren <bingtimren@gmail.com>
2018-08-24 02:26:46 +1000

    fix: correct default plugin path from module (#4)

--- template/lib/module.js
+++ template/lib/module.js
@@ -4,8 +4,9 @@ module.exports = async function module (moduleOptions) {
   // const options = Object.assign({}, moduleOptions)
 
   // this.addPlugin({
-  //   src: resolve(__dirname, '../templates/plugin.js'),
+  //   src: resolve(__dirname, 'templates/plugin.js'),
   //   fileName: '{{ name }}.js',
   //   options
   // })
 }
+module.exports.meta = require('../package.json')
