b9e33b0897413fc25b9fb03b318f9b175ea94be1
pooya parsa <pooya@pi0.ir>
2019-02-12 11:21:21 +0330

    cleaner default module.js

--- template/lib/module.js
+++ template/lib/module.js
@@ -1,14 +1,13 @@
-// const { resolve } = require('path')
+const { resolve } = require('path')
 
-async function nuxtModule(moduleOptions) {
-  // const options = Object.assign({}, moduleOptions)
+module.exports = async function (moduleOptions) {
+  const options = Object.assign({}, moduleOptions)
 
-  // this.addPlugin({
-  //   src: resolve(__dirname, 'plugin.js'),
-  //   fileName: '{{ name }}.js',
-  //   options
-  // })
+  this.addPlugin({
+    src: resolve(__dirname, 'plugin.js'),
+    fileName: '{{ name }}.js',
+    options
+  })
 }
 
-module.exports = nuxtModule
 module.exports.meta = require('../package.json')
