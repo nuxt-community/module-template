cc787e9f4dc55f88009cf1f42d78bd414a48b979
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-02-12 18:53:32 -0200

    test(module): require `get-port`

--- template/test/module.test.js
+++ template/test/module.test.js
@@ -2,6 +2,7 @@ jest.setTimeout(60000)
 
 const { Nuxt, Builder } = require('nuxt-edge')
 const request = require('request-promise-native')
+const getPort = require('get-port')
 
 const config = require('../example/nuxt.config')
 
