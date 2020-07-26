139d82fdf5b8106f4196102e974e5cad06639345
Daniel Roe <daniel@concision.co.uk>
2020-07-27 00:15:39 +0100

    fix: don't collect coverage from templates

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: '@siroc/jest-preset',
   collectCoverage: true,
-  collectCoverageFrom: ['dist/**', 'templates/**']
+  collectCoverageFrom: ['dist/**', '!templates/**']
 }
