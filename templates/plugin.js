efa5298c45a9415640d6ffa7673836aff1a4d41b
Daniel Roe <daniel@roe.dev>
2020-08-05 22:28:47 +0100

    fix: simplify `templates/plugin.js`

--- templates/plugin.js
+++ templates/plugin.js
@@ -1,8 +1,6 @@
 /**
  * @type {import('@nuxt/types').Plugin}
  */
-const plugin = async function ({ app, store }, inject) {
+export default async function ({ app, store }, inject) {
   //
 }
-
-export default plugin
