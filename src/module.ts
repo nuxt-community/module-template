f0be49643d145e5a5b27e439e48fb441609f5053
Daniel Roe <daniel@roe.dev>
2021-03-08 14:10:14 +0000

    refactor: various module template improvements
    * use `upath` for better windows support
    * inline package.json name/version
    * type options as as Partial by default
    * give example of typed access to normalised nuxt options
    * correct `defu` type error
    * add package exports

--- src/module.ts
+++ src/module.ts
@@ -1,14 +1,19 @@
-import { resolve } from 'path'
+import { resolve } from 'upath'
 import defu from 'defu'
-import { Module } from '@nuxt/types'
+
+import { Module, NuxtOptions } from '@nuxt/types'
+
+import { name, version } from '../package.json'
 
 export interface ModuleOptions {}
 const DEFAULTS: ModuleOptions = {}
 const CONFIG_KEY = 'myModule'
 
 const nuxtModule: Module<ModuleOptions> = /* async */ function (moduleOptions) {
-  const options = defu<ModuleOptions>(this.options[CONFIG_KEY], moduleOptions, DEFAULTS)
-  // const { nuxt } = this
+  const options = defu(this.options[CONFIG_KEY] /* istanbul ignore next */ || {}, moduleOptions, DEFAULTS)
+
+  const nuxtOptions: NuxtOptions = this.nuxt.options
+  console.log('[remove] Normalized & typed Nuxt options', nuxtOptions)
 
   this.addPlugin({
     src: resolve(__dirname, '../templates/plugin.js'),
@@ -17,11 +22,11 @@ const nuxtModule: Module<ModuleOptions> = /* async */ function (moduleOptions) {
   })
 }
 
-;(nuxtModule as any).meta = require('../package.json')
+;(nuxtModule as any).meta = { name, version }
 
 declare module '@nuxt/types' {
-  interface NuxtConfig { [CONFIG_KEY]?: ModuleOptions } // Nuxt 2.14+
-  interface Configuration { [CONFIG_KEY]?: ModuleOptions } // Nuxt 2.9 - 2.13
+  interface NuxtConfig { [CONFIG_KEY]?: Partial<ModuleOptions> } // Nuxt 2.14+
+  interface Configuration { [CONFIG_KEY]?: Partial<ModuleOptions> } // Nuxt 2.9 - 2.13
 }
 
 export default nuxtModule
